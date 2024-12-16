fdd18fb6ed6b359a3672fb79cd7adfa1786c58b1
Fatim Dicko <102673545+Anna062@users.noreply.github.com>
2024-12-16 16:04:42 +0100

    Accolade manquante

--- codes/pj1_1_led/pj1_1_led.ino
+++ codes/pj1_1_led/pj1_1_led.ino
@@ -31,9 +31,11 @@ void loop() {
     analogWrite(led_y, i);  //PWM output, control LED brightness
     delay(3);
   }
-  for (int i = 255; i > 0; i--)  //The for loop statement continues to decrease the value of variable i until it exits the loop at 0 
+  for (int i = 255; i > 0; i--) //The for loop statement continues to decrease the value of variable i until it exits the loop at 0 
+  {  
     analogWrite(led_y, i);
     delay(3);
+  }
 }
 
 
